1079d50793654ff969a9331a3271d0f7b3c0671e
Christian Gintenreiter <christian.gintenreiter@gmail.com>
2026-02-10 03:20:06 +0100

    release: v0.3.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Dspy.MixProject do
   def project do
     [
       app: :dspy,
-      version: "0.3.3",
+      version: "0.3.4",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps()
